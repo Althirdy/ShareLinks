cd773ec566b7c70a5f02b3645f9a9e91e4b5b94f
Althirdy <althirdysanger@gmail.com>
2024-07-25 09:33:02 +0800

    Update: {LinkMobileView.jsx}: adding a React skeleton in the preview of the profile

--- src/Components/LinksMobileView.jsx
+++ src/Components/LinksMobileView.jsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react'
 import LinkView from './LinkView'
 import { LinksContext } from 'src/Context/ApiContext'
 import { defaultImg } from 'src/Data/Platform'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 
 export default function LinksMobileView() {
     const { linkCards, profile } = useContext(LinksContext)
@@ -15,14 +18,14 @@ export default function LinksMobileView() {
                         className={`h-36 mb-5 w-36 shadow-lg bg-gray-300 rounded-full ${profile.imgSrc === defaultImg && animate}`}>
                         <img src={profile.imgSrc} alt="" className='h-full w-full rounded-full object-cover relative z-10' />
                     </div>
-                    {profile.FullName ?
-                        <h3 className='text-2xl text-center font-bold tracking-tight text-gray-900'>{profile.FullName}</h3> :
-                        <div className={`h-2 w-44 rounded-md bg-gray-300 animate-pulse my-2`}></div>
-                    }
-                    {profile.Email ?
-                        <h3 className='text-md font-medium text-center tracking-tight text-gray-500 mt-[-1px]'>{profile.Email}</h3> :
-                        <div className={`w-40 h-2 rounded-md bg-gray-300 animate-pulse`}></div>
-                    }
+                    <h3 className='text-2xl text-center font-bold tracking-tight text-gray-900' >
+                        {profile.FullName || <Skeleton width={200} baseColor='#cfcaca' />}
+                    </h3>
+                    <div>
+                        <h3 className='text-md font-medium text-center tracking-tight text-gray-500 mt-[-1px]'>
+                            {profile.Email || <Skeleton width={180} baseColor='#cfcaca' />}
+                        </h3>
+                    </div>
                 </div>
                 <h5 className='text-lg font-medium'>About {profile.FullName}</h5>
                 <textarea value={profile.Description} rows={10} disabled className='text-sm font-regular p-2  bg-transparent resize-none w-full tracking-tight text-gray-800 mt-3 rounded-md  ' />
